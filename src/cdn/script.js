f80e9c9d7d38fa852f9ff60901ce331f75a3717a
PICKLE <98971557+pickle69420@users.noreply.github.com>
2023-01-20 20:24:24 +0000

    successdully implemented tooltip

--- src/cdn/script.js
+++ src/cdn/script.js
@@ -10,6 +10,35 @@ function piklload() {
         jamlib.rel = 'stylesheet';
         jamlib.href = 'https://unpkg.com/jam-icons/css/jam.min.css'
         document.head.appendChild(jamlib);
+        fetch('./cdn/apps.json')
+        .then((response) => response.json())
+        .then((data) => {
+            for (let i = 0; i < data.length; i++) {
+                var taskbar = document.getElementsByClassName('pikl-taskbar')[0];
+                var div = document.createElement('div');
+                var app = document.createElement('span');
+                var tooltip = document.getElementsByClassName('pikl-tasktooltip')[0];
+                div.classList.add('pikl-taskicon');
+                app.classList.add('jam');
+                app.classList.add(data[i].icon);
+                taskbar.appendChild(div);
+                div.appendChild(app);
+                div.addEventListener('mouseenter', (event) => {
+                    tooltip.innerHTML = data[i].displayname + `<div></div>`;
+                    tooltip.style.setProperty('--tw-scale-x','1');
+                    tooltip.style.setProperty('--tw-scale-y','1');
+                    if(event.target.classList.contains('pikl-taskicon')) {
+                        event.target.prepend(tooltip);
+                    } else {
+                        event.target.parentNode.prepend(tooltip);
+                    }
+                });
+                div.addEventListener('mouseleave', (event) => {
+                    tooltip.style.setProperty('--tw-scale-x','0');
+                    tooltip.style.setProperty('--tw-scale-y','0');
+                });
+            }
+        });
         resolve();
     });
 }
