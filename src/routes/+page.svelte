7a4d34845a10923f6f8d4521e45d4aff4e5b2690
pickle69420 <andreilivshits@gmail.com>
2023-02-28 21:08:35 +0000

    coverup for no reason

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,1 @@
-<button on:click={incrementCount}>HEloo</button>
-<p>{count} is {doubled}</p>
-<h1 class="text-3xl color-blue-500">HEloo</h1>
-
-<script>
-    let count = 0;
-    $: doubled = count * 2;
-    function incrementCount() {
-        count += 1;
-    }
-</script>
+<h1>Welcome to EduLive, a school app that is transforming the education spectrum!</h1>
