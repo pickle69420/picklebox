2e0486db6e0640e70f8e64cebb6c6b015930a55d
Andrei Livshits <98971557+pickle69420@users.noreply.github.com>
2023-03-02 04:00:13 +0000

    upgrade uv

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,7 +1,7 @@
 /*global UVServiceWorker*/
-importScripts("/uv/bundle.js");
-importScripts("/uv/config.js");
-importScripts("/uv/sw.js");
+importScripts("/uv/uv.bundle.js");
+importScripts("/uv/uv.config.js");
+importScripts("/uv/uv.sw.js");
 
 const sw = new UVServiceWorker();
 
