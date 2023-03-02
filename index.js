d20aeca2263fd5bbbfae6bee597ec5885625de2c
Andrei Livshits <98971557+pickle69420@users.noreply.github.com>
2023-03-02 03:22:37 +0000

    just a small change that added the bare server

---
+++ index.js
@@ -0,0 +1,36 @@
+import createBareServer from '@tomphttp/bare-server-node';
+import express from 'express';
+import http from 'node:http';
+import { handler } from './build/handler.js';
+
+const httpServer = http.createServer();
+
+const app = express();
+
+app.use(handler);
+
+const bareServer = createBareServer('/bare/');
+
+httpServer.on('request', (req, res) => {
+	if (bareServer.shouldRoute(req)) {
+		bareServer.routeRequest(req, res);
+	} else {
+		app(req, res);
+	}
+});
+
+httpServer.on('upgrade', (req, socket, head) => {
+	if (bareServer.shouldRoute(req)) {
+		bareServer.routeUpgrade(req, socket, head);
+	} else {
+		socket.end();
+	}
+});
+
+httpServer.on('listening', () => {
+	console.log('PickleHub listening on port 3000');
+});
+
+httpServer.listen({
+	port: 3000,
+});
