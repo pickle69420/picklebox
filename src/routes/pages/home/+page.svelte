0cb8921d6f02fc0f64a8f18fa45477407475ada3
PICKLE <98971557+pickle69420@users.noreply.github.com>
2023-03-31 13:33:09 -0700

    Update +page.svelte

--- src/routes/pages/home/+page.svelte
+++ src/routes/pages/home/+page.svelte
@@ -2,26 +2,27 @@
     import '../../cdn/jam/jam.min.css';
     import { onMount } from 'svelte';
     onMount(async () => {
-    if ("serviceWorker" in navigator) {
-      const registrations = await navigator.serviceWorker.getRegistrations();
-      if (registrations.length <= 0) {
         if ("serviceWorker" in navigator) {
-          navigator.serviceWorker
-            .register("/service-worker.js", {
-              scope: "/~uv/"
-            })
-            .then(() => {
-              location.reload();
-            });
+          const registrations = await navigator.serviceWorker.getRegistrations();
+          if (registrations.length <= 0) {
+            if ("serviceWorker" in navigator) {
+              navigator.serviceWorker
+                .register("/sw.js", {
+                  scope: "/~uv/"
+                })
+                .then(() => {
+                  location.reload();
+                });
+            }
+          } else {
+            if ("serviceWorker" in navigator) {
+              navigator.serviceWorker.register("/sw.js", {
+                scope: "/~uv/"
+              });
+            }
+          }
         }
-      } else {
-        if ("serviceWorker" in navigator) {
-          navigator.serviceWorker.register("/service-worker.js", {
-            scope: "/~uv/"
-          });
-        }
-      }
-    }});
+    });
 </script>
 
 <h1 class="pikl-apptitle"><span class="jam jam-home"></span>Home</h1>
